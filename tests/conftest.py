ed15c031c0fcb4b3d414c1f3e548ddf0f19dde5b
avishee19 <avishee066@gmail.com>
2026-09-17 19:50:37 +0530

    Configure Chrome for CI execution

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,15 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver():
-    browser = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    browser = webdriver.Chrome(options=options)
+
     browser.get("https://www.saucedemo.com/")
 
     yield browser
